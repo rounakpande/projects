0f53c09a5cca0c337f49b1a52a443fdcb70727b4
rounakpande <rounakpande21@gmail.com>
2024-10-07 19:21:37 +0530

    TO-DO List
    I have created a TO-DO list using html css and javascript.
    This TO-List provides feature such as:
       1.Add a task
       2.Delete a task
       3.Mark and Unmark a task.

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask() {
+    if (inputbox.value === '') {
+        alert("Add a task!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+}
+
+listcontainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
